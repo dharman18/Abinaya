4cdf6f2fbc24b2dbedb2347299de92bfd034a88f
voicecollect <110962384+voicecollect@users.noreply.github.com>
2022-09-16 20:37:20 +0200

    Create tag.service.spec.ts

---
+++ tag.service.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+import { TagService } from './tag.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+
+describe('TagService', () => {
+  let service: TagService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        imports:[HttpClientTestingModule],
+        providers:[TagService]
+    });
+
+    service = TestBed.inject(TagService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
